fd5964bbc94413d253c49c8907ae6f7bd0ca5645
Samantha brake <sammy.brake@gmail.com>
2018-09-18 20:55:24 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
   end 
   
   def add_item(title, price, quantity = 1)
-      last_transaction = price * quantity 
+      @last_transaction = price * quantity 
     @total += price * quantity
  
   end 
