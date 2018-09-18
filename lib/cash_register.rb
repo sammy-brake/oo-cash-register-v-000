14784dbc9121a270fde401ec3a6f2ed83e436786
Samantha brake <sammy.brake@gmail.com>
2018-09-18 20:06:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,10 @@
 class CashRegister
   attr_accessor :total 
   
-  def initialize(total = 0)
+  def initialize(total = 0, discount = 0)
     @total = total 
   end 
   
-  def discount
-    @total/20
-  end 
+ 
     
 end 
