24117bab0974172ed8c8dcb06e603481702f19f9
Samantha brake <sammy.brake@gmail.com>
2018-09-18 21:19:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ class CashRegister
   def apply_discount
 if @discount > 0
       @discount = @discount/100.to_f
-      binding.pry 
+ 
       @total = @total - (@total * @discount)
     
       puts "After the discount, the total comes to #{@total}."
